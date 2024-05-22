0c8b171223506c405630e93fcb29e1da44f3e78a
mohammad-haj <mohammadhag44@gmail.com>
2024-05-22 20:45:13 +0300

    task7-tast2-1

---
+++ assest/javascrirt/script.js
@@ -0,0 +1,9 @@
+function changeBackgroundColor1 (){
+    document.body.style.backgroundColor = "red"
+}
+function changeBackgroundColor2 (){
+    document.body.style.backgroundColor = "blue"
+}
+function changeBackgroundColor3 (){
+    document.body.style.backgroundColor = "green"
+}
